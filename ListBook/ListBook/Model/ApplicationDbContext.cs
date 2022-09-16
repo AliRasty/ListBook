2973288a6d48c9827118162d540a432e185d97a3
AliRasty <Alirasty1997@gmail.com>
2022-09-16 00:31:12 -0700

    Start Project And Add Connection string

---
+++ ListBook/ListBook/Model/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ListBook.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
